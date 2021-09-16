6d707cb36f81edf5519c24668c06b78a2a03f8b7
Tobias Worledge <tobiasworledge@gmail.com>
2021-09-16 00:59:35 -0700

    Made training more efficient. Implemented time-trot simulation. Changed model data format

--- new_idea.py
+++ new_idea.py
@@ -1,4 +1,4 @@
-from test_model import optimize_winrate
+from test_model import optimize_winrate, random_turn, get_best_turn, get_key
 from opponent_brain import opponent_moves
 #from player_moves_incomp import player_moves_incomplete
 
@@ -9,19 +9,22 @@ PLAYER_MAX_SCORE = 50
 OPPONENT_MAX_SCORE = 50
 
 USING_INCOMPLETE_FILE = False
-
 if USING_INCOMPLETE_FILE:
     player_moves = player_moves_incomplete
 else:
-    player_moves = [[-1 for rows in range(PLAYER_MAX_SCORE)] for cols in range(OPPONENT_MAX_SCORE)]
+    player_moves = [[{} for rows in range(PLAYER_MAX_SCORE)] for cols in range(OPPONENT_MAX_SCORE)]
+
+#USING DEFAULT OPPONENT MOVES
+opponent_moves = 0
 
 counter = 0
 for current_opponent_score in range(OPPONENT_MAX_SCORE - 1, -1, -1):
     for current_player_score in range(PLAYER_MAX_SCORE - 1, -1, -1):
-        if player_moves[current_player_score][current_opponent_score] == -1:
+        if len(player_moves[current_player_score][current_opponent_score]) == 0:
             player_moves = optimize_winrate(player_moves, opponent_moves, current_player_score, current_opponent_score, PLAYER_MAX_SCORE, OPPONENT_MAX_SCORE)
         counter += 1
         print("GAMESTATE:", counter)
+        print("Weights:", player_moves[current_player_score][current_opponent_score])
 
     version += 1
     storage_file = open(file_name + str(version) + ".txt", "w")
--- test_model.py
+++ test_model.py
@@ -12,36 +12,40 @@ def test_model(first_move=0, use_first_move=False, player_actions=4, opponent_ac
         if player_score_start == 0 and opponent_score_start == 0:
             turn_counter = 0
         else:
-            random_index = random.randint(len(trot[player_score_start + opponent_score_start]))
-            turn_counter = trot[player_score_start + opponent_score_start][random_index]
+            turn_counter = random_turn(player_score_start, opponent_score_start)
         first_turn = True
         take_extra_turn = False
+        unknown_first_turn = turn_counter == -1
+        enable_time_trot = False
         while True:
             if first_turn and use_first_move:
                 first_turn = False
                 player_move = first_move
             else:
-                player_move = player_actions[playerScore][opponentScore]
-                if player_move == -1:
-                    print("ERROR, move is -1")
+                player_move = get_best_turn(player_actions[playerScore][opponentScore])
+                if player_move == -1: print("ERROR, move is -1")
             
-            if player_move == turn_counter:
+            if player_move == turn_counter and not unknown_first_turn:
                 take_extra_turn = True
             
             playerScore, opponentScore, won = take_turn(playerScore, opponentScore, player_move, max_score=player_max_score)
             turn_counter += 1
 
-            if take_extra_turn: #Implemented only for player atm
+            if take_extra_turn and not won and enable_time_trot: #Implemented only for player atm
                 take_extra_turn = False
-                player_move = player_actions[playerScore][opponentScore]
+                player_move = get_best_turn(player_actions[playerScore][opponentScore])
                 playerScore, opponentScore, won = take_turn(playerScore, opponentScore, player_move, max_score=player_max_score)
+                turn_counter += 1
 
             if won:
                 wins_player += 1
                 break
             
+            #DETERMINE OPPONENT'S MOVE
             #opponent_move = opponent_actions[opponentScore][playerScore]
             opponent_move = random.randint(4,6)
+            #DETERMINE OPPONENT'S MOVE
+
             opponentScore, playerScore, won = take_turn(opponentScore, playerScore, opponent_move, max_score=opponent_max_score)
             turn_counter += 1
 
@@ -51,24 +55,64 @@ def test_model(first_move=0, use_first_move=False, player_actions=4, opponent_ac
 
     return wins_player / test_rounds
 
+def test_model_v2(first_move=0, player_actions=4, opponent_actions=4, player_score_start=0, opponent_score_start=0, player_max_score=50, opponent_max_score=50, test_rounds=100000):
+    total_winrate = 0
+    for _ in range(test_rounds):
+        result_winrate = 0
+        playerScore = player_score_start
+        opponentScore = opponent_score_start
+
+        current_turn = random_turn(player_score_start, opponent_score_start)
+        unknown_first_turn = current_turn == -1
+
+        take_extra_turn = False
+        enable_time_trot = True
+
+        player_move = first_move #Determine player move
+        if player_move == current_turn and not unknown_first_turn:
+            take_extra_turn = True
+        
+        playerScore, opponentScore, player_won = take_turn(playerScore, opponentScore, player_move, max_score=player_max_score)
+
+        if take_extra_turn and not player_won and enable_time_trot: #Implemented only for player atm
+            player_move = get_best_turn(player_actions[playerScore][opponentScore])
+            playerScore, opponentScore, player_won = take_turn(playerScore, opponentScore, player_move, max_score=player_max_score)
+
+        if not player_won:
+            #DETERMINE OPPONENT'S MOVE
+            #opponent_move = opponent_actions[opponentScore][playerScore]
+            opponent_move = random.randint(4,6)
+            #DETERMINE OPPONENT'S MOVE
+
+            opponentScore, playerScore, opponent_won = take_turn(opponentScore, playerScore, opponent_move, max_score=opponent_max_score)
+
+        #Calculating resulting winrate of action
+        if player_won:
+            result_winrate = 1
+        elif opponent_won:
+            result_winrate = 0
+        else:
+            if playerScore > 49 or opponentScore > 49:
+                print("Player score too high")
+            
+            result_winrate = get_best_winrate(player_actions[playerScore][opponentScore])
+        
+        total_winrate += result_winrate
+
+    return total_winrate / test_rounds
 
 def optimize_winrate(player_moves, opponent_moves, player_score_start, opponent_score_start, player_max_score, opponent_max_score):
-    highest_winrate = 0
-    best_move = 0
 
-    if player_score_start < 10 and opponent_score_start < 10:
-        test_rounds = 100000
+    if abs(player_score_start - opponent_score_start) > 30:
+        test_rounds = 10000
     elif player_score_start < 35 and opponent_score_start < 35:
         test_rounds = 100000
     else:
         test_rounds = 10000
 
     for move in range(0, 11):
-        current_winrate = test_model(move, True, player_moves, opponent_actions=opponent_moves, player_score_start=player_score_start, opponent_score_start=opponent_score_start, player_max_score=player_max_score, opponent_max_score=opponent_max_score, test_rounds=test_rounds)
-        if current_winrate > highest_winrate:
-            highest_winrate, best_move = current_winrate, move
-    
-    player_moves[player_score_start][opponent_score_start] = best_move
+        current_winrate = test_model_v2(move, player_moves, opponent_actions=opponent_moves, player_score_start=player_score_start, opponent_score_start=opponent_score_start, player_max_score=player_max_score, opponent_max_score=opponent_max_score, test_rounds=test_rounds)
+        player_moves[player_score_start][opponent_score_start][move] = current_winrate
 
     return player_moves
 
@@ -115,6 +159,11 @@ def get_turn_estimate(player_actions=4, opponent_actions=4, player_max_score=50,
     return score_turn_average
 
 def random_turn(player_score, opponent_score):
+    if not len(trot[player_score][opponent_score]):
+        return -1
+    elif sum(trot[player_score][opponent_score].values()) < 200:
+        return -1
+    
     random_value = random.randint(1, sum(trot[player_score][opponent_score].values()))
     selected_turn = -1
     while random_value > 0:
@@ -125,4 +174,13 @@ def random_turn(player_score, opponent_score):
             pass
     return selected_turn
 
-print(random_turn(32, 38))
+def get_key(dict, val):
+    for i in dict:
+        if dict[i] == val:
+            return i
+
+def get_best_turn(dict):
+    return get_key(dict, max(dict.values()))
+
+def get_best_winrate(dict):
+    return max(dict.values())
